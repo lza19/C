087b9d076f8d3a66678ad2e4350d1422d0c09748
lza19 <maxx5611056110@gmail.com>
2025-12-17 23:47:59 +0700

    2d

--- test/testbefor/test1/1.c
+++ test/testbefor/test1/1.c
@@ -2,7 +2,22 @@
 
 int main (){
 
-    
+
+    int x;
+    int i;
+
+    for (i=0; i<5; i++){
+        printf("กรอกเลขรอบที่ %d: ", i+1);
+        scanf("%d", &x);
+        printf("Number: %d\n", x);
+    }
+
+
     printf("Hello, World!\n");
     return 0;
+    do {
+
+    }while (i<10);
+   
+    
 }
